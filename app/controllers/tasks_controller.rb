31b1b0a89e6975ecf15a6f0761474044d858f51e
akari.shikota <moon39.cho45@gmail.com>
2021-07-15 12:59:28 +0000

    change2

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,7 +1,7 @@
 class TasksController < ApplicationController
     before_action :require_user_logged_in
-    before_action :correct_user
-    
+    before_action :correct_user, only:  [:edit, :show, :update, :destroy]
+
     def index
         @tasks = current_user.tasks.order(id: :desc).page(params[:page])
     end
