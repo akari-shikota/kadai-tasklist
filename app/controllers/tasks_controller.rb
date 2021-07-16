ae63ce249fd96675cc60d05dc28b4c273e091a89
akari.shikota <moon39.cho45@gmail.com>
2021-07-16 14:26:46 +0000

    changed

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,13 +1,13 @@
 class TasksController < ApplicationController
     before_action :require_user_logged_in
-    before_action :correct_user, only:  [:edit, :show, :update, :destroy]
+    before_action :correct_user, only:  [:show, :edit, :update, :destroy]
 
     def index
         @tasks = current_user.tasks.order(id: :desc).page(params[:page])
     end
     
     def show
-        @task = Task.find(params[:id])
+        
     end
     
     def new
@@ -27,11 +27,10 @@ class TasksController < ApplicationController
     end
     
     def edit
-        @task = Task.find(params[:id])
+        
     end
     
     def update
-        @task = Task.find(params[:id])
         if @task.update(task_params)
           flash[:success] = 'タスクは正常に更新されました'
           redirect_to @task
--- db/schema.rb
+++ db/schema.rb
@@ -12,14 +12,6 @@
 
 ActiveRecord::Schema.define(version: 2021_07_14_051652) do
 
-  create_table "tasklists", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "content"
-    t.bigint "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_tasklists_on_user_id"
-  end
-
   create_table "tasks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "content"
     t.datetime "created_at", null: false
@@ -37,6 +29,5 @@ ActiveRecord::Schema.define(version: 2021_07_14_051652) do
     t.datetime "updated_at", null: false
   end
 
-  add_foreign_key "tasklists", "users"
   add_foreign_key "tasks", "users"
 end
